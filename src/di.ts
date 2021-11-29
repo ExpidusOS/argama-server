fe266a26282b0bdde0cd6227e424594bbbbe0342
RossComputerGuy <rosscomputerguy@protonmail.com>
2021-11-29 15:15:33 -0800

    chore: added server and process uptime info

--- src/di.ts
+++ src/di.ts
@@ -3,8 +3,10 @@ import { init as dbInit } from './database'
 
 export const DI = {} as {
 	orm: MikroORM
+	server_start: Date
 }
 
 export async function init(): Promise<void> {
 	DI.orm = await dbInit()
+	DI.server_start = new Date()
 }
--- src/http/controllers/v1/instance.ts
+++ src/http/controllers/v1/instance.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from 'express'
 import { BaseMessage } from '../../../message'
 import config from '../../../config'
+import { DI } from '../../../di'
 import { version, repository, homepage, devDependencies, author, license } from '../../../../package.json'
+import process from 'process'
 
 export default function() {
 	return {
@@ -9,6 +11,10 @@ export default function() {
 			try {
 				res.json(new BaseMessage({
 					implements: ['v1'],
+					uptime: {
+						server: Date.now() - DI.server_start.getTime(),
+						process: process.uptime()
+					},
 					repository,
 					homepage,
 					devDependencies,
